ae0db3cc76ddc73df35da42f7d4660915a466867
Christoph Lansche <christoph.lansche@t-online.de>
2020-11-29 17:57:17 +0100

    add use Illuminate\Support\Facades\Schema; and Schema::defaultStringLength(191); to AppServiceProvider.php

--- database/archive/2020_10_24_190032_create_incredient_quantity_table.php
+++ database/archive/2020_10_24_190032_create_incredient_quantity_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIncredientsQuantitiesTable extends Migration
+class CreateIncredientQuantityTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateIncredientsQuantitiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('incredients_quantities', function (Blueprint $table) {
+        Schema::create('incredient_quantity', function (Blueprint $table) {
             $table->foreignId('incredient_id');
             $table->foreignId('quantity_id');
         });
