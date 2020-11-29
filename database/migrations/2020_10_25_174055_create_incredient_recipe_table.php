c38a597817ea217ccd696501d9ce2c608ba2c7c9
Christoph Lansche <christoph.lansche@t-online.de>
2020-11-29 17:52:52 +0100

    change migration for incredient_recipe_table to be migrated after units_table

---
+++ database/migrations/2020_10_25_174055_create_incredient_recipe_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncredientRecipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incredient_recipe', function (Blueprint $table) {
+            $table->foreignId('incredient_id')->onDelete('cascade')->constrained();
+            $table->foreignId('recipe_id')->onDelete('cascade')->constrained();
+            $table->double('quantity', 4, 1)->nullable();
+            $table->foreignId('unit_id')->nullable()->cascadeOnDelete()->constrained();
+            $table->boolean('main_incredient')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incredients_recipes');
+    }
+}
